50c3a53f2f6f0e4bde1566db5e1ea1192ff96812
RyanPeszko <ryan.peszko@gmail.com>
2022-10-03 21:44:06 +1100

    feat: Storybook integration test

---
+++ stories/pages/HomePage.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import HomePage from "../../pages/index";
+
+export default {
+    title: "Pages/HomePage",
+    component: HomePage,
+  };
+
+export const Home = () => <HomePage />
